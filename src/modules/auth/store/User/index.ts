1dc317710b64653b3fb8b9a0eb01147baec08bc0
JuniorAlbuquerque <junioralbuquerque81@gmail.com>
2022-10-16 16:44:19 -0300

    chore: create user store

---
+++ src/modules/auth/store/User/index.ts
@@ -0,0 +1,40 @@
+import { Login_login, Login_login_user } from '@/graphql/generated/Login'
+import create from 'zustand'
+import { persist } from 'zustand/middleware'
+
+type User = Omit<Login_login_user, '__typename'>
+
+type UseUserStore = Omit<Login_login, '__typename'> & {
+  updateToken?: (token: string) => void
+  updateUser?: (user: User) => void
+}
+
+export const useUserStore = create(
+  persist<UseUserStore>(
+    (set) => ({
+      user: null,
+      token: null,
+      updateToken: (token: string) => {
+        set(() => ({
+          token
+        }))
+      },
+      updateUser: (user: User) => {
+        set(() => ({
+          user
+        }))
+      }
+    }),
+    {
+      name: '@Clinica-Sabrina',
+      partialize: (state) => {
+        if (state.token) {
+          return {
+            token: state.token,
+            user: state.user
+          }
+        }
+      }
+    }
+  )
+)
