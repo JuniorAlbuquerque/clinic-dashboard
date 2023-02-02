4e3945eb6c6414c8ac98af966e0f018e813f2d37
Junior Albuerque <junioralbuquerque81@gmail.com>
2023-02-01 23:44:59 -0400

    styles: disabled style in input component

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -1,10 +1,11 @@
+import clsx from 'clsx'
 import { FC, InputHTMLAttributes, useId } from 'react'
 
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   label?: string
 }
 
-export const Input: FC<InputProps> = ({ label, ...rest }) => {
+export const Input: FC<InputProps> = ({ label, disabled, ...rest }) => {
   const currentId = useId()
 
   return (
@@ -18,7 +19,13 @@ export const Input: FC<InputProps> = ({ label, ...rest }) => {
       <div className="mt-1">
         <input
           id={currentId}
-          className="py-2 pl-3 pr-10block w-full rounded-md border border-gray-300 shadow-sm focus:border-primary-500 focus:outline-none focus:ring-1 focus:ring-primary-500 sm:text-sm"
+          className={clsx(
+            'py-2 pl-3 pr-10block w-full rounded-md border border-gray-300 shadow-sm focus:border-primary-500 focus:outline-none focus:ring-1 focus:ring-primary-500 sm:text-sm',
+            {
+              'bg-gray-100': disabled
+            }
+          )}
+          disabled={disabled}
           {...rest}
         />
       </div>
