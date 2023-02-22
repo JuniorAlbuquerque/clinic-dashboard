be7afbe5e76df01fed6b75b06dac0b015571fdfc
Junior Albuerque <junioralbuquerque81@gmail.com>
2023-02-21 23:52:31 -0400

    fix: tooltip type

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,8 +1,8 @@
 import clsx from 'clsx'
 import { FC } from 'react'
-import { Tooltip as ReactTooltip, ITooltipController } from 'react-tooltip'
+import { Tooltip as ReactTooltip, ITooltip } from 'react-tooltip'
 
-const Tooltip: FC<ITooltipController> = ({ children, ...rest }) => {
+const Tooltip: FC<ITooltip> = ({ children, ...rest }) => {
   return (
     <ReactTooltip
       place="right"
